e0fa014dfc1e42fd5d5d13244d883329d4c242d9
Stiward20 <114256800+Stiward20@users.noreply.github.com>
2023-05-25 16:14:27 -0500

    Add files via upload

---
+++ arreglo/src/Array/Principal.java
@@ -0,0 +1,9 @@
+package Array;
+
+public class Principal {
+    public static void main(String[] args) {
+        ArregloImc misArregloImc1 = new ArregloImc();
+        misArregloImc1.iniciar();
+    }
+    
+}
